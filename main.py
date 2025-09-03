55e696804cbcfb98af7af53dfbd798df474e267b
maty <maty.idk@emai.cz>
2025-09-03 23:12:34 +0200

    shuffled questions and correct numbering of questions

--- main.py
+++ main.py
@@ -31,11 +31,9 @@ for key, value in questions.items():
         copy_list[index] = value["options"][i]
         i += 1
 
-    print(f"Key: {key}\nValue: {value}")
     value["options"] = copy_list
-    print(f"Key: {key}\nValue: {value}")
 
-    # Correct numbering of question
+    # Correct numbering of answers
     letter = 68
     i = len(value["options"]) - 1
     while i >= 0:
@@ -44,5 +42,14 @@ for key, value in questions.items():
         i -= 1
         letter -= 1
 
+
+items = list(questions.items())
+random.shuffle(items)
+questions = dict(items)
+
+i = 1
+for key, value in questions.items():
+    key = str(i) + key[2:]
+
+    i += 1
     print(f"Key: {key}\nValue: {value}")
-    break
