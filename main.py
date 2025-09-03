ab987678526b0e5661449a7c59a6a549ccec7f4d
maty <maty.idk@emai.cz>
2025-09-03 22:27:47 +0200

    shuffle 'bin' num

--- main.py
+++ main.py
@@ -10,24 +10,29 @@ with open("zasobnik_otazek.txt", "r", encoding="utf-8") as f:
                 key = line
                 questions[key] = {
                     "options": list(),
-                    "answer": 0b1000
+                    "answer": "1000"
                 }
             else:
                 questions[key]["options"].append(line.strip())
         except:
             continue
 
-for key, value in questions.items():
-    print(f"Key: {key}\nValue: {bin(value)}")
-
 # for key, value in questions.items():
-#     random.shuffle(value)
-#
-#     letter = 68
-#     i = len(value) - 1
-#     while i >= 0:
-#         value[i] = chr(letter) + value[i][1:]
-#
-#         i -= 1
-#         letter -= 1
-#     print(f"Key: {key}\nValue: {value}")
+#     print(f"Key: {key}\nValue: {bin(value)}")
+
+for key, value in questions.items():
+    to_shuffle = list(str(value["answer"]))
+
+    random.shuffle(to_shuffle)
+
+    value["answer"] = "".join(to_shuffle)
+
+
+    # letter = 68
+    # i = len(value) - 1
+    # while i >= 0:
+    #     value[i] = chr(letter) + value[i][1:]
+    #
+    #     i -= 1
+    #     letter -= 1
+    # print(f"Key: {key}\nValue: {value}")
