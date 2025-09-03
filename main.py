6d82775fafbe3ea34b21299247ff3ffc68bf5f26
maty <maty.idk@emai.cz>
2025-09-03 21:39:11 +0200

    add bin number to identify which answer is right

--- main.py
+++ main.py
@@ -8,20 +8,26 @@ with open("zasobnik_otazek.txt", "r", encoding="utf-8") as f:
             line = line.strip()
             if line[0].isdigit():
                 key = line
-                questions[key] = list()
+                questions[key] = {
+                    "options": list(),
+                    "answer": 0b1000
+                }
             else:
-                questions[key].append(line.strip())
+                questions[key]["options"].append(line.strip())
         except:
             continue
 
 for key, value in questions.items():
-    random.shuffle(value)
+    print(f"Key: {key}\nValue: {bin(value)}")
 
-    letter = 68
-    i = len(value) - 1
-    while i >= 0:
-        value[i] = chr(letter) + value[i][1:]
-
-        i -= 1
-        letter -= 1
-    print(f"Key: {key}\nValue: {value}")
+# for key, value in questions.items():
+#     random.shuffle(value)
+#
+#     letter = 68
+#     i = len(value) - 1
+#     while i >= 0:
+#         value[i] = chr(letter) + value[i][1:]
+#
+#         i -= 1
+#         letter -= 1
+#     print(f"Key: {key}\nValue: {value}")
