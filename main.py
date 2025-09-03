8b71cea2d97c12f9dce436b96e1237242011b203
maty <maty.idk@emai.cz>
2025-09-03 12:19:44 +0200

    2nd version, remove unused funcitons

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 ﻿questions = {}
 
 with open("zasobnik_otazek.txt", "r", encoding="utf-8") as f:
-    i = 0
     for line in f:
-        if line[0].isalnum():
-            try:
-                if line[0].isdigit():
-                    key = line.strip()
-                    questions[key] = list()
-                else:
-                    questions[key].append(line.strip())
-            except:
-                continue
+        try:
+            line = line.strip()
+            if line[0].isdigit():
+                key = line
+                questions[key] = list()
+            else:
+                questions[key].append(line.strip())
+        except:
+            continue
 
 for key, value in questions.items():
     print(f"Key: {key}\nValue: {value}")
