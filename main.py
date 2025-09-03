14a27321198a0804af90f2a347ade9e4c8781d3c
maty <maty.idk@emai.cz>
2025-09-03 12:16:50 +0200

    main.py txt to dict 1st version

---
+++ main.py
@@ -0,0 +1,17 @@
+﻿questions = {}
+
+with open("zasobnik_otazek.txt", "r", encoding="utf-8") as f:
+    i = 0
+    for line in f:
+        if line[0].isalnum():
+            try:
+                if line[0].isdigit():
+                    key = line.strip()
+                    questions[key] = list()
+                else:
+                    questions[key].append(line.strip())
+            except:
+                continue
+
+for key, value in questions.items():
+    print(f"Key: {key}\nValue: {value}")
