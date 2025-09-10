54ba4aa6903207b13daad00bf23cfd09bc1c5eae
maty <maty.idk@emai.cz>
2025-09-10 23:01:11 +0200

    rework, it didnt work, i forgot what happaned

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-﻿import random
-import re
+﻿import re
 
-questions = {}
+questions = []
 
 with open("zasobnik_otazek.txt", "r", encoding="utf-8") as f:
+    i = -1
     for line in f:
-        question = re.search(r"^\d.*[:?]$", line)
+        question = re.search(r"^\d.*[:?]$", line) # load question
+        if question: questions.append( {"question": question.group()} ); i += 1 # check if it's not None
 
-        if question:
-            question = question.group()
-            questions[question] = list()
-            current_question = question
+        answer = re.search(r"^[A-D][)].*[.]$", line) # load answer
 
+        if answer: questions[i].setdefault("options", []).append(answer.group()) # check if it's not None
 
-        answer = re.search(r"^\w[)].*[.]$", line)
-        if answer: questions[current_question].append(answer.group())
-
-for k, v in questions.items():
-    random.shuffle(questions[k])
-
-for k, v in questions.items():
-    print(k + "\n" + "\n".join(v) + "\n")
+for question in questions: print(question)
