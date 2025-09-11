49f0408c13416d5cc19692c4544ecc988d4ce168
bag1s3k <jetam.mravenec@gmail.com>
2025-09-11 16:49:42 +0200

    add guess logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ﻿import random
 import re
+from termcolor import cprint
 
 questions = []
 
@@ -18,9 +19,30 @@ for question in questions:
 
     i = 0
     for option in question["options"]:
-        if option.startswith("A"): question["correct"] = i
+        if option.startswith("A"): question["correct"] = chr(i + 65)
         i += 1
 
     question["options"] = [chr(i + 65) + option[1:] for i, option in enumerate(question["options"])]
 
-for question in questions: print(question)
+wrong, correct = {}, 0
+for question in questions:
+    print("\n" + question["question"])
+
+    for option in question["options"]: print(option)
+
+    guess = input("> ").upper().strip()
+
+    if guess == "STOP": break
+    elif guess == question["correct"]: correct += 1
+    else:
+        wrong[question["question"]] = {
+            "right": question["options"][ord(question["correct"]) - 65],
+            "wrong": question["options"][ord(guess) - 65]
+        }
+
+print(f"Score: {correct}/100")
+
+for key, value in wrong.items():
+    print(f"\n{key}\n")
+    cprint(value["right"], "green", attrs=["bold"])
+    cprint(value["wrong"], "red", attrs=["bold"])
