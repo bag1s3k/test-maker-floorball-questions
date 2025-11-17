160a5c82612d7075834a2efb12ca43d50a2db29a
bag1s3k <jetam.mravenec@gmail.com>
2025-11-17 13:21:55 +0100

    Code should be more readable

--- main.py
+++ main.py
@@ -8,18 +8,22 @@ with open("zasobnik_otazek.txt", "r", encoding="utf-8") as f:
     i = -1
     for line in f:
         question = re.search(r"^\d.*[:?]$", line) # load question
-        if question: questions.append( {"question": question.group()} ); i += 1 # check if it's not None
+        if question:
+            questions.append( {"question": question.group()} )
+            i += 1
 
         answer = re.search(r"^[A-D][)].*[.]$", line) # load answer
 
-        if answer: questions[i].setdefault("options", []).append(answer.group()) # check if it's not None
+        if answer:
+            questions[i].setdefault("options", []).append(answer.group())
 
 for question in questions:
     random.shuffle(question["options"])
 
     i = 0
     for option in question["options"]:
-        if option.startswith("A"): question["correct"] = chr(i + 65)
+        if option.startswith("A"):
+            question["correct"] = chr(i + 65)
         i += 1
 
     question["options"] = [chr(i + 65) + option[1:] for i, option in enumerate(question["options"])]
@@ -32,12 +36,15 @@ wrong, correct = {}, 0
 for question in questions:
     print("\n" + question["question"])
 
-    for option in question["options"]: print(option)
+    for option in question["options"]:
+        print(option)
 
     guess = input("> ").upper().strip()
 
-    if guess == "STOP": break
-    elif guess == question["correct"]: correct += 1
+    if guess == "STOP":
+        break
+    elif guess == question["correct"]:
+        correct += 1
     else:
         wrong[question["question"]] = {
             "right": question["options"][ord(question["correct"]) - 65],
